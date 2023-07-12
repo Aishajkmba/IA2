dda3f9e8bd5303c9d513c777f485a6430e0a7df9
17jokomba_a <17jokomba_a@bexleygs.co.uk>
2023-07-12 16:13:35 +0100

    clicking

--- src/GUImain.java
+++ src/GUImain.java
@@ -1,18 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 // provides a way to create new graph pop-ups
 public class GUImain extends JFrame implements ActionListener {
     //private JFrame frame;
     private JButton newGraph;
+    private ArrayList<JButton> allButtons;
+    private String copyText;
 
 
     public GUImain() {
         //frame = new JFrame("Circular Flow of Income Diagram");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //set size of JFrame
-        setSize(400, 400);
+        setSize(2000, 2000);
         setLayout(null);
         JButton button1;
         JButton button2;
@@ -27,12 +30,28 @@ public class GUImain extends JFrame implements ActionListener {
         JButton button11;
         JButton button12;
         JButton button13;
+        JButton button14;
+        JButton button15;
+        JButton button16;
+        JButton button17;
+        JButton button18;
+        JButton button19;
+        JButton button20;
+        JButton button21;
+        JButton button22;
+        JButton button23;
+        JButton button24;
+        JButton button25;
+        JButton button26;
+
 
 
         int height = 0;
         int width = 0;
         this.setPreferredSize(new Dimension(width, height));
         setLayout(null);
+        JPanel panel = new JPanel(null);
+
         button1 = new JButton("Circular Flow of Income Diagram");
         button1.setBounds(350, 100, 250, 40);
         button2 = new JButton("households");
@@ -59,6 +78,60 @@ public class GUImain extends JFrame implements ActionListener {
         button12.setBounds(350, 375, 250, 40);
         button13 = new JButton("Income(wages, profit, rent, interest)");
         button13.setBounds(350, 400, 250, 40);
+        button14 = new JButton("");
+        button14.setBounds(350, 100, 250, 40);
+        button15 = new JButton("");
+        button15.setBounds(350, 125, 100, 40);
+        button16 = new JButton("");
+        button16.setBounds(350, 150, 100, 40);
+        button17 = new JButton("");
+        button17.setBounds(350, 175, 250, 40);
+        button18 = new JButton("");
+        button18.setBounds(350, 200, 100, 40);
+        button19 = new JButton("");
+        button19.setBounds(350, 225, 100, 40);
+        button20 = new JButton("");
+        button20.setBounds(350, 250, 100, 40);
+        button21 = new JButton("");
+        button21.setBounds(350, 275, 100, 40);
+        button22 = new JButton("");
+        button22.setBounds(350, 300, 100, 40);
+        button23 = new JButton("");
+        button23.setBounds(350, 325, 250, 40);
+        button24 = new JButton("");
+        button24.setBounds(350, 350, 250, 40);
+        button25 = new JButton("");
+        button25.setBounds(350, 375, 250, 40);
+        button26 = new JButton("");
+        button26.setBounds(350, 400, 250, 40);
+
+        button1.setName("0");
+        button2.setName("1");
+        button3.setName("0");
+        button4.setName("1");
+        button5.setName("0");
+        button6.setName("1");
+        button7.setName("0");
+        button8.setName("1");
+        button9.setName("0");
+        button10.setName("1");
+        button11.setName("0");
+        button12.setName("1");
+        button13.setName("0");
+        button14.setName("1");
+        button15.setName("0");
+        button16.setName("1");
+        button17.setName("0");
+        button18.setName("1");
+        button19.setName("0");
+        button20.setName("1");
+        button21.setName("0");
+        button22.setName("1");
+        button23.setName("0");
+        button24.setName("1");
+        button25.setName("0");
+        button26.setName("1");
+
 
 
         add(button1);
@@ -87,8 +160,34 @@ public class GUImain extends JFrame implements ActionListener {
         button12.addActionListener(this);
         add(button13);
         button13.addActionListener(this);
+        add(button14);
+        button14.addActionListener(this);
+        add(button15);
+        button15.addActionListener(this);
+        add(button16);
+        button16.addActionListener(this);
+        add(button17);
+        button17.addActionListener(this);
+        add(button18);
+        button18.addActionListener(this);
+        add(button19);
+        button19.addActionListener(this);
+        add(button20);
+        button20.addActionListener(this);
+        add(button21);
+        button21.addActionListener(this);
+        add(button22);
+        button22.addActionListener(this);
+        add(button23);
+        button23.addActionListener(this);
+        add(button24);
+        button24.addActionListener(this);
+        add(button25);
+        button25.addActionListener(this);
+        add(button26);
+        button26.addActionListener(this);
         setVisible(true);
-        getContentPane();
+        getContentPane().add(panel);
 
 
         System.out.println("SEQUENCE: GUI constructor");
@@ -120,6 +219,34 @@ public class GUImain extends JFrame implements ActionListener {
         button12.setBounds(350, 375, 100, 40);
         button13 = new JButton("Income(wages, profit, rent, interest)");
         button13.setBounds(350, 400, 250, 40);
+        button14 = new JButton("");
+        button14.setBounds(350, 100, 250, 40);
+        button15 = new JButton("");
+        button15.setBounds(350, 125, 100, 40);
+        button16 = new JButton("");
+        button16.setBounds(350, 150, 100, 40);
+        button17 = new JButton("");
+        button17.setBounds(350, 175, 250, 40);
+        button18 = new JButton("");
+        button18.setBounds(350, 200, 100, 40);
+        button19 = new JButton("");
+        button19.setBounds(350, 225, 100, 40);
+        button20 = new JButton("");
+        button20.setBounds(350, 250, 100, 40);
+        button21 = new JButton("");
+        button21.setBounds(350, 275, 100, 40);
+        button22 = new JButton("");
+        button22.setBounds(350, 300, 100, 40);
+        button23 = new JButton("");
+        button23.setBounds(350, 325, 250, 40);
+        button24 = new JButton("");
+        button24.setBounds(350, 350, 250, 40);
+        button25 = new JButton("");
+        button25.setBounds(350, 375, 250, 40);
+        button26 = new JButton("");
+        button26.setBounds(350, 400, 250, 40);
+
+
 
 
         add(button1);
@@ -148,6 +275,62 @@ public class GUImain extends JFrame implements ActionListener {
         button12.addActionListener(this);
         add(button13);
         button13.addActionListener(this);
+        button14.addActionListener(this);
+        add(button15);
+        button15.addActionListener(this);
+        add(button16);
+        button16.addActionListener(this);
+        add(button17);
+        button17.addActionListener(this);
+        add(button18);
+        button18.addActionListener(this);
+        add(button19);
+        button19.addActionListener(this);
+        add(button20);
+        button20.addActionListener(this);
+        add(button21);
+        button21.addActionListener(this);
+        add(button22);
+        button22.addActionListener(this);
+        add(button23);
+        button23.addActionListener(this);
+        add(button24);
+        button24.addActionListener(this);
+        add(button25);
+        button25.addActionListener(this);
+        add(button26);
+        button26.addActionListener(this);
+
+
+
+
+
+        panel.add(button1);
+        panel.add(button2);
+        panel.add(button3);
+        panel.add(button4);
+        panel.add(button5);
+        panel.add(button6);
+        panel.add(button7);
+        panel.add(button8);
+        panel.add(button9);
+        panel.add(button10);
+        panel.add(button11);
+        panel.add(button12);
+        panel.add(button13);
+        panel.add(button14);
+        panel.add(button15);
+        panel.add(button16);
+        panel.add(button17);
+        panel.add(button18);
+        panel.add(button19);
+        panel.add(button20);
+        panel.add(button22);
+        panel.add(button23);
+        panel.add(button24);
+        panel.add(button25);
+        panel.add(button26);
+        panel.add(button21);
 
     }
 
